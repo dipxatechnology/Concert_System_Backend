ddeb91ae1698a3e0322165416b64c3606fe6aa3d
Abdullah al Sakib <abdullahalsakib903@gmail.com>
2024-01-02 01:29:21 +0800

    adding pipe validation features to the backend

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,27 +1,18 @@
-// Import necessary modules from Nest.js
-import { Controller, Get } from '@nestjs/common';
-import * as fs from 'fs';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { log } from 'console';
 
-@Controller('example')
-export class ExampleController {
-  @Get('json')
-  getJsonFile(): any {
-    try {
-      // Specify the path to your JSON file
-      const filePath = './data.json';
-
-      // Read the JSON file
-      const jsonData = fs.readFileSync(filePath, 'utf-8');
-
-      // Parse the JSON data
-      const parsedData = JSON.parse(jsonData);
-
-      // Return the parsed data
-      return parsedData;
-    } catch (error) {
-      // Handle errors, e.g., file not found, JSON parse error
-      console.error('Error reading JSON file:', error.message);
-      return { error: 'Error reading JSON file' };
-    }
+@Controller('users')
+export class UsersController {
+  @Get()
+  listAllUsers() {
+    return 'adam, akkif';
+  }
+  @Post()
+  addUsers(@Body() body: any) {
+    console.log(body);
+  }
+  @Get('/:id')
+  getUsers(@Param('id') id: string) {
+    console.log(id);
   }
 }
